54145e39ffe315000d01d080871900097a8b462f
RyanAVelasco <45979367+RyanAVelasco@users.noreply.github.com>
2021-08-01 19:47:46 -0500

    Added GUI
    Used tkinter to add a GUI with scrollbar.
    GUI contains episode links that not in the archive file.

--- cr_episode_link_retrieval.py
+++ cr_episode_link_retrieval.py
@@ -1,9 +1,14 @@
 import os
 
 import colored
-from colored import stylize
+import tkinter
+# import tkMessageBox
 
+from colored import stylize
+from tkinter import *
 from selenium import webdriver
+
+
 
 ## This program is meant to to complement youtube-dl, populating a text file with series episode links. It is also specifically for use with Crunchyroll and because using series links leads to near 100% errors when trying to download. Using episode links sidesteps this problem and never (for me anyways) led to errors.
 
@@ -539,8 +544,28 @@ for episode in episode_list.readlines():
 #     print(e)
 # quit()
 
+root = Tk()
+root.title('Crunchyroll: New Episodes')
+
+# Creates a window within root so we can attach the scrollbar.
+frame = Frame(root)
+frame.pack()
+
+# We're showing links written we're using a listbox and placing is on the left of frame.
+listbox = Listbox(frame, width=125, height=50)
+listbox.pack(side=LEFT)
+
+# Creating the scrollbar and placing is on the right of frame.
+sb = Scrollbar(frame, orient=VERTICAL)
+sb.pack(side=RIGHT, fill=Y)
+
+# Setting up the scrollbar for use.
+listbox.configure(yscrollcommand=sb.set)
+sb.config(command=listbox.yview)
+
 with webdriver.Firefox(options=fireFoxOptions) as driver:
     count = 0
+    list_entry = 0
     for series in cvl:
         driver.get(cvl[count])
 
@@ -554,19 +579,48 @@ with webdriver.Firefox(options=fireFoxOptions) as driver:
             episode_link = episode_link[1]
             
             ## Prevents writing duplicate links by checking current_episode list for it
-            if episode_link + '\n' in current_episode:
+            if episode_link + '\n' in current_episode: 
+                # Used only for showing duplicate entries, you won't need it on
                 print(stylize('[ARCHIVED] ' + episode_link, colored.fg(196)))
                 continue
 
             ## Note: entries will not appear in archive until the entire specified series has been written
-            ## If attribute is an episode link then print and write to cvel
+            ## If attribute is an episode link then print and write to cvel.
             elif parse_episode_link.startswith(cvl[count] + '/episode'):
+
+                # Appends episode link to the cr_episode_list.txt.
                 episode_list = open(crunchyroll_episodes, 'a')
-                episode_list.write(episode_link + '\n')
+                episode_list.write(episode_link + '\n') 
+                
+                # Used only for showing whats been written, you won't need it on
                 print(stylize('[WRITING] ' + episode_link, colored.fg(112)))
+
+                # Below: Formatting for new episodes displayed using tkinter for aesthetic purpose only, you can safely comment this out.
+
+                # Removes the episode ID.
+                episode_link = episode_link[:-7]
+                
+                 # Splits series title from episode.
+                episode_link = episode_link.split('/')
+
+                series_title = episode_link[0].replace('-', ' ').title()
+
+                # Replace the hyphen after episode number with a semi-colon
+                episode_title = episode_link[1].replace('-', ': ', 2).replace('-', ' ').title()
+
+                #Finalizes the formatting for display in list_entry
+                episode_link = series_title + ' - ' + episode_title
+
+                # Above: Formatting for new episodes displayed using tkinter for aesthetic purpose only, you can safely comment this out.
+
+                #Inserts episode link into listbox and is displayed once the program has finished going through the entirety of cvl
+                listbox.insert(list_entry, episode_link)                
                 
         ## Incrementing begins next series to load
         count+=1
+        list_entry += 1
+        listbox.pack(padx=5, pady= 10)
     driver.quit
-    quit()
+    # quit()
+root.mainloop()
     
